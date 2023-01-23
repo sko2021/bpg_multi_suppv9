b61cc69007f5e8e324df3a35cd4c8f5bc2ce29f5
brijesh54 <brijeshvasoya54@gmail.com>
2023-01-24 00:49:57 +0530

    update with logout

--- static/scripts/bpg.js
+++ static/scripts/bpg.js
@@ -26,28 +26,3 @@ catch(error){
 	console.log("Redirecting to AAD Logout")
     location.href = 'logout';
 }
-
-function logout2(app_name) {
-    console.log(`Initiating Logout For Application ${app_name}`);
-    try {
-        for (i = 0; i < logoutUrls.length; i++) {
-        console.log("Calling URL[" + i + "]: " + logoutUrls[i]);
-        // if (app_name==logoutUrls.)
-        $.ajax({
-            url: logoutUrls[i],
-            type: "GET",
-			async: false,
-        	cache: false,
-        	timeout: 30000,
-            success: function(data, status, jqXHR) {
-                console.log("Status[" + i + "]: " +  jqXHR.status);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        })
-    }
-}
-catch(error){
-    console.log(error);
-}}
